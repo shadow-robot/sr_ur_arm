9346a50e6f5d243b6c2f37ce17ac1437b4a5a614
Manos Nikolaidis <manos@shadowrobot.com>
2014-10-27 21:05:05 +0000

    Update sr_ur_program_loader.cpp

--- sr_ur_controller/src/sr_ur_program_loader.cpp
+++ sr_ur_controller/src/sr_ur_program_loader.cpp
@@ -33,19 +33,20 @@
 char *robot_program_path;
 
 static uv_connect_t connect_to_robot_request;
-static uv_write_t send_file_request;
-static uv_tcp_t send_file_stream;
-static uv_buf_t file_buffer;
-static uv_fs_t file_request;
+static uv_write_t   send_file_request;
+static uv_tcp_t     send_file_stream;
+static uv_buf_t     file_buffer;
+static uv_fs_t      file_request;
 
 static char *file_path;
 static bool main_program_currently;
 
-static int local_port;
+// host server will listen on this port for the robot client to connect
+static int host_port;
 
 static void load_file_from_disk();
 
-// this is called after the reset robot program has been sent
+// this is called after the robot program has been sent
 static void file_sent_cb(uv_write_t* send_file_request, int status)
 {
   ROS_ASSERT(send_file_request);
@@ -68,21 +69,28 @@ static void file_sent_cb(uv_write_t* send_file_request, int status)
   }
 }
 
+// prepare the robot program to send by format printing host address and port
 static void prepare_file_buffer()
 {
   if (!main_program_currently)
+  {
     return;
+  }
 
   char *temp_buffer;
-  size_t allocated_size = asprintf(&temp_buffer, file_buffer.base, host_address, local_port);
+  size_t allocated_size = asprintf(&temp_buffer, file_buffer.base, host_address, host_port);
   ROS_ASSERT(allocated_size > 0);
 
   free(file_buffer.base);
   allocated_size = asprintf(&file_buffer.base, "%s", temp_buffer);
+  free(temp_buffer);
+  
   ROS_ASSERT(allocated_size > 0);
   file_buffer.len = allocated_size;
 }
 
+// connected to the server on the robot that will receive the robot program
+// so start sending the program
 static void client_connected_cb(uv_connect_t* connection_request, int status)
 {
   ROS_ASSERT(connection_request);
@@ -99,6 +107,8 @@ static void client_connected_cb(uv_connect_t* connection_request, int status)
   ROS_ASSERT(0 == status);
 }
 
+// disk file containing the robot program has been loaded
+// so connect to server in robot to send
 static void file_loaded_cb(uv_fs_t *load_file_request)
 {
   ROS_ASSERT(load_file_request);
@@ -111,6 +121,8 @@ static void file_loaded_cb(uv_fs_t *load_file_request)
   ROS_ASSERT(0 == status);
 }
 
+// opened the disk file containing the robot program
+// so read the file
 static void file_opened_cb(uv_fs_t *open_file_request)
 {
   ROS_ASSERT(open_file_request);
@@ -128,8 +140,11 @@ static void file_opened_cb(uv_fs_t *open_file_request)
   ROS_ASSERT(0 == status);
 }
 
+// load disk file containing robot program
 static void load_file_from_disk()
 {
+  // the main program is ur_robot_program that is doing the actual work
+  // but ur_reset_program must be sent before that 
   const char *file_name = main_program_currently ? "ur_robot_program" : "ur_reset_program";
   free(file_path);
   int status = asprintf(&file_path, "%s%s", robot_program_path, file_name);
@@ -139,7 +154,10 @@ static void load_file_from_disk()
   struct stat st;
   size_t file_size = 0;
   if (0 == stat(file_path, &st))
+  {
     file_size = st.st_size;
+  }
+  
   file_buffer.base = (char*)realloc((void*)file_buffer.base, file_size);
   file_buffer.len = file_size;
 
@@ -154,9 +172,10 @@ static void load_file_from_disk()
   ROS_ASSERT(0 == status);
 }
 
+// load first a reset and then a main robot program to the robot
 void load_robot_program(int reverse_port)
 {
-  local_port = reverse_port;
+  host_port = reverse_port;
   load_file_from_disk();
 
   // initialise stream for client that writes a robot program
