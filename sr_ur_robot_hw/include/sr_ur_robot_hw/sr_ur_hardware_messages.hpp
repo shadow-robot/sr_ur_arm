50de2b3a99763970a9514a9903a63ad29b562278
Ethan Fowler <ethan@shadowrobot.com>
2019-08-30 17:11:48 +0000

    Copyright fixes.

--- sr_ur_robot_hw/include/sr_ur_robot_hw/sr_ur_hardware_messages.hpp
+++ sr_ur_robot_hw/include/sr_ur_robot_hw/sr_ur_hardware_messages.hpp
@@ -1,24 +1,23 @@
-/* Copyright (c) 2014, Shadow Robot Company, All rights reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
- */
-
-/**
- * @file   sr_ur_hardware_messages.hpp
- * @author Manos Nikolaidis <manos@shadowrobot.com>
- * @brief  messages the driver may send to the ur arm controller
- **/
+/*
+* Copyright 2014 Shadow Robot Company Ltd.
+*
+* This program is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+* @file   sr_ur_hardware_messages.hpp
+* @author Manos Nikolaidis <manos@shadowrobot.com>
+* @brief  messages the driver may send to the ur arm controller
+**/
 
 #ifndef SR_UR_HARDWARE_MESSAGES_HPP_
 #define SR_UR_HARDWARE_MESSAGES_HPP_
